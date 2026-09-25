8df10960ce60d81b8ff9f21d36b9aa7b22e1605a
alkayyalirahaf <alkayyalirahaf2000@gmail.com>
2026-09-25 15:03:42 +0300

    Add a required validation

--- Registration/app.js
+++ Registration/app.js
@@ -1,10 +1,11 @@
 
 
 //Select
-const form  = document.querySelector("form");
+// const form  = document.querySelector("form");
 const userNameInput=document.getElementById("userName");
 const passwordInput=document.getElementById("password");
-const confirmPassword=document.getElementById("confirmPassword");
+const confirmPasswordInput=document.getElementById("confirmPassword");
+const registerBtn=document.getElementById("regBtn");
 
 //create
 const userNameLabel=document.createElement("label");
@@ -20,4 +21,33 @@ confirmPasswordLabel.textContent="Confirm Password";
 //append 
 userNameInput.before(userNameLabel);
 passwordInput.before(passwordLabel);
-confirmPassword.before(confirmPasswordLabel);
+confirmPasswordInput.before(confirmPasswordLabel);
+
+
+//required validation 
+registerBtn.addEventListener("click",(e) =>{
+    e.preventDefault();
+
+    if(userNameInput.value===""){
+        const error = document.createElement("span");
+        error.textContent="user name is required";
+        userNameInput.after(error);
+    }
+
+
+
+    if(passwordInput.value===""){
+        const error = document.createElement("span");
+        error.textContent="Password is required";
+        passwordInput.after(error);
+    }
+
+ if(confirmPasswordInput.value===""){
+        const error = document.createElement("span");
+        error.textContent="confirm Password is required";
+        confirmPasswordInput.after(error);
+    }
+
+
+
+})
