12e78a84b05e5c0ff86cbb28274532b684c6d660
alkayyalirahaf <alkayyalirahaf2000@gmail.com>
2026-09-25 12:25:22 +0300

    Create a rendering function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const membershipInput = document.getElementById("Membership");
 const genreInput = document.getElementById("genre");
 const bookTitleInput = document.getElementById("bookTitle");
 
+const resultCard= document.getElementById("result-card");
 
 
 
@@ -20,6 +21,17 @@ submitBtn.addEventListener("click", (event) => {
 
 
     const student = [userName,memberShip, bookGenre, bookTitle];
+    renderingStudent(student);
 
    }
 });
+
+
+function renderingStudent(student){
+    for (let i=0;i<student.length;i++){
+        const para = document.createElement("p");
+        para.innerHTML=student[i];
+        resultCard.appendChild(para);
+    }
+    
+}
