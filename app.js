0273fdf9e563861f47999ba6a21005dcc88b1ec1
alkayyalirahaf <alkayyalirahaf2000@gmail.com>
2026-09-25 12:07:51 +0300

    store the validated data in array

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const submitBtn = document.getElementById("submitBtn");
 
+const userNameInput = document.getElementById("userName");
+const membershipInput = document.getElementById("Membership");
+const genreInput = document.getElementById("genre");
+const bookTitleInput = document.getElementById("bookTitle");
+
+
 
 
 submitBtn.addEventListener("click", (event) => {
     event.preventDefault();
 
-    console.log("Button clicked");
+    const userName = userNameInput.value;
+    const memberShip = membershipInput.value;
+    const bookGenre = genreInput.value;
+    const bookTitle = bookTitleInput.value;
+
+   if (memberShip === "student" || memberShip === "regular"){
+
+
+    const student = [userName,memberShip, bookGenre, bookTitle];
+
+   }
 });
