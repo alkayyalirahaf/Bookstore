69743b0a7616b609f03a0de937fd48a70bff4a8a
alkayyalirahaf <alkayyalirahaf2000@gmail.com>
2026-09-26 10:01:39 +0300

    Highlight words longer than 8 characters

---
+++ Exercisemid/app.js
@@ -0,0 +1,13 @@
+const paragraph = document.getElementById("paragraph");
+const text=paragraph.textContent;
+const words = text.split(" ");
+
+
+for(let i =0;i<words.length;i++){
+    if(words[i].length>8){
+            words[i] = '<span style="background-color: yellow;">' + words[i] + '</span>';
+
+
+    }
+}
+paragraph.innerHTML = words.join(" ");
