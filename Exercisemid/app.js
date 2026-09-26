bc2f157e173fae8d827a66e2e677e34980a441b5
alkayyalirahaf <alkayyalirahaf2000@gmail.com>
2026-09-26 10:08:11 +0300

    Add source link after paragraph

--- Exercisemid/app.js
+++ Exercisemid/app.js
@@ -2,7 +2,7 @@ const paragraph = document.getElementById("paragraph");
 const text=paragraph.textContent;
 const words = text.split(" ");
 
-
+//Highlight words longer than 8 characters
 for(let i =0;i<words.length;i++){
     if(words[i].length>8){
             words[i] = '<span style="background-color: yellow;">' + words[i] + '</span>';
@@ -11,3 +11,9 @@ for(let i =0;i<words.length;i++){
     }
 }
 paragraph.innerHTML = words.join(" ");
+
+
+//Add source link after paragraph
+const link = document.createElement("a");
+link.textContent="https://google.com/";
+paragraph.appendChild(link);
